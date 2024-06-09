7d1ef52f665b070272ad77f6a058eeb53eb6d146
Alvin Nuha <alvinn549@gmail.com>
2024-06-09 17:20:44 +0700

    refactor: improve phone number formatting in WablasNotification

--- app/Http/Services/WablasNotification.php
+++ app/Http/Services/WablasNotification.php
@@ -31,16 +31,10 @@ class WablasNotification
 
         if (substr($phone, 0, 1) === '0') {
             $phone = '62' . substr($phone, 1);
-        } elseif (substr($phone, 0, 2) === '62' && strlen($phone) === 11) {
-            return $phone;
-        } elseif (substr($phone, 0, 3) === '628' && strlen($phone) === 12) {
-            $phone = '62' . substr($phone, 3);
-        } elseif (substr($phone, 0, 4) === '6288' && strlen($phone) === 13) {
-            $phone = '62' . substr($phone, 4);
-        } elseif (substr($phone, 0, 5) === '62888' && strlen($phone) === 14) {
-            $phone = '62' . substr($phone, 5);
+        } elseif (substr($phone, 0, 2) !== '62') {
+            $phone = '62' . $phone;
         } else {
-            return null;
+            $phone = null;
         }
 
         return $phone;
